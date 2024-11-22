1d174f1e3590a2be931f945546418dc3c0816b4b
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-22 09:26:50 +0700

    b1

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+const { data, pending, error } = await useAsyncData("posts", () =>
+    $fetch("https://jsonplaceholder.typicode.com/posts")
+);
+</script>
+
 <template>
     <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+        <h1>Danh sách bài viết</h1>
+        <ul v-if="data">
+            <li v-for="post in data" :key="post.id">
+                <h3>{{ post.title }}</h3>
+                <p>{{ post.body }}</p>
+            </li>
+        </ul>
+        <div v-else-if="pending">Đang tải dữ liệu...</div>
+        <div v-else>Có lỗi xảy ra khi tải dữ liệu.</div>
     </div>
 </template>
